3628965e887c3d7633fcdd4f61f4dfe2e9c028a1
mingpeach <mjb940806@naver.com>
2016-12-06 13:56:57 +0900

    savemail

---
+++ likelion_savemail/app/controllers/home_controller.rb
@@ -0,0 +1,42 @@
+require 'mailgun'
+
+class HomeController < ApplicationController
+    
+    def write
+        
+    end
+    
+    def complete
+        @to = params[:email]
+        @title = params[:title]
+        @from = params[:email2]
+        @content = params[:content]
+        
+        mg_client = Mailgun::Client.new("key-7093b51f2f16e2d3a84868e4e0aef758")
+
+        message_params =  {
+                   from: @from,
+                   to:   @to,
+                   subject: @title,
+                   text:    @content
+                  }
+
+        result = mg_client.send_message('sandbox8e6d7ffae14a435cbdf5e65ef9b2082a.mailgun.org', message_params).to_h!
+
+        message_id = result['id']
+        message = result['message']
+        
+        new_post = Post.new
+        new_post.to = @to
+        new_post.title = @title
+        new_post.content = @content
+        new_post.save
+        
+        redirect_to "/list"        
+    end
+
+    def list
+        
+        @every_post = Post.all
+    end    
+end
